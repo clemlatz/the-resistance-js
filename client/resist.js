5d7bbb2930dc192188683b582fd3c3ec14a0da25
Clement Bourgoin <cb@nokto.net>
2015-01-03 10:50:51 +0100

    Move client files in /client

--- client/resist.js
+++ client/resist.js
@@ -7,24 +7,24 @@ function Game() {
 
 Game.prototype = {
 	start: function() {
-	
+
 		// Prompt for number of player
 		var numPlayers = 0
 		while (numPlayers < 5 || numPlayers > 10) {
 			numPlayers = prompt("How many players?", 5);
 		}
 		
-		
+
 		// Choose rule according to number
 		this.rule = rules[numPlayers];
-		
+
 		// Get each player's name
 		var name = undefined;
 		for (var i = 0; i < numPlayers; i++) {
 			name = prompt("What is your name, player "+(i+1)+"?", "Player"+(i+1));
 			this.players.push(new Player(name, i));
 		}
-		
+
 		// Set spies
 		var spyCount = 0;
 		while (spyCount < this.rule.spies) {
@@ -34,77 +34,77 @@ Game.prototype = {
 				spyCount++;
 			}
 		}
-		
+
 		this.showRoles();
 	},
 	showRoles: function() {
-		
+
 		for (var i = 0, c = this.players.length; i < c; i++) {
 			var player = this.players[i];
 			var role = player.isSpy ? "Spy" : "Resistant";
-			
+
 			alert("Please hand the device to "+player.name+".\nIf you are "+player.name+", press OK.");
 			alert(player.name+", your role is: "+role+".\nPress OK before you hand the device.");
 		}
-		
+
 		this.recognition();
-		
+
 	},
 	recognition: function() {
-		
+
 		alert("Spy recognition phase will begin. Be sure your speaker volume is up and press OK.");
-		
+
 		// Every player close their eyes
 		say('To all players: please close your eyes.', 1000);
-		
+
 		// Spies, open your eyes
 		say('Spies: please open your eyes.', 6000);
-		
+
 		// Spies, close your eyes
 		say('Spies: please close your eyes.', 11000);
-		
+
 		// Every player open their eyes
 		say('To all players: you may now open your eyes.', 14000);
-		
+
 		var _this = this;
 		setTimeout(function() {
 			_this.main();
 		}, 14000)
-		
+
 	},
 	main: function() {
-	
-		
+
+
 		var nextLeaderKey = 1;
 		var leader = this.players[0];
 		var nextLeader = this.players[nextLeaderKey];
-		
+
 		var success = 0,
 			failures = 0;
-		
+
 		// 5 mission loop
 		for (var missionNumber = 0; missionNumber < 5; missionNumber++) {
-			
+
 			var vote = false,
 				leadersRemaining = 5;
-			
+
 /* 			if (confirm("Exit?")) throw "Game exited"; */
 			alert("New mission!\n\nMission "+(missionNumber+1)+" of 5\nLeader: "+leader.name+"\nNext leader: "+nextLeader.name+"\n");
-			
+
 			// Vote for leader choice acceptance
 			while (!vote) {
 				alert(leader.name+", choose "+this.rule.missions[missionNumber]+" soldiers for this mission.");
 				var vote = confirm(leader.name+", do the majority of players accept your choice of soldiers for this mission?");
-			
+
 				if (!vote) {
-				
+
 					leadersRemaining--;
-					if(leadersRemaining == 0) 
+					if(leadersRemaining == 0)
 					{
 						alert("Game over! You ran out of leaders. The governement wins.");
 						throw "Game over!";
 					}
-					
+
 					// Change leader & nextLeader
 					leader = nextLeader;
 					nextLeaderKey++;
@@ -113,7 +113,7 @@ Game.prototype = {
 					alert("New leader for mission"+(missionNumber+1)+"\n\nThe new leader is: "+leader.name+"\nNext leader will be: "+nextLeader.name+"\n\nLeaders remaining: "+leadersRemaining);
 				}
 			}
-			
+
 			// Add player to team
 			var team = [];
 			for (var i = 0; team.length < this.rule.missions[missionNumber]; i++) {
@@ -123,9 +123,9 @@ Game.prototype = {
 					team.push(player);
 				}
 			}
-			
+
 			alert("Mission begins!");
-			
+
 			// Each player vote for mission success
 			var sabotages = 0;
 			for (var i = 0, c = team.length; i < c; i++) {
@@ -135,7 +135,7 @@ Game.prototype = {
 					sabotages++;
 				}
 			}
-			
+
 			// Mission result
 			var missionSuccess = true;
 			if (missionNumber == 4 && this.players.length > 6)
@@ -149,7 +149,7 @@ Game.prototype = {
 			{
 				missionSuccess = false;
 			}
-			
+
 			if (!missionSuccess) {
 				failures++;
 				var result = "Failure!";
@@ -158,18 +158,18 @@ Game.prototype = {
 				var result = "Success!";
 			}
 			alert("Mission result: "+result+" ("+sabotages+" sabotages)");
-			
+
 			// If the spies have 3 points, they win
 			if (failures == 3) {
 				alert("Game over! The governement wins.");
 				throw "Game over! The resistance wins.";
 			}
-			
+
 			// Change leader & nextLeader
 			leader = nextLeader;
 			nextLeaderKey = nextLeaderKey >= this.players.length ? 0 : nextLeaderKey+1;
 			nextLeader = this.players[nextLeaderKey];
-			
+
 		}
 		alert("Game over! The resistance wins.");
 	}
@@ -223,12 +223,3 @@ function newGame() {
 	var game = new Game();
 	game.start();
 }
-
-
-
-
-
-
-
-
-
