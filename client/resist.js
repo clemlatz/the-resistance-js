ad8bb195355e261723457e928bcf57ff7050a275
Clement Bourgoin <cb@nokto.net>
2015-01-04 03:24:49 +0100

    Revamp app using AngularJS

--- client/resist.js
+++ client/resist.js
@@ -1,185 +1,297 @@
-function Game() {
-	this.players = [];
+// Initialize Angular app
+var app = angular.module('resist', ['uuid4']);
+
+// Models
+function Game(id) {
+	this.id = id;
+	this.log = [];
 	this.started = new Date();
-	this.phase = "start";
 	this.rule = undefined;
-}
-
-Game.prototype = {
-	start: function() {
-
-		// Prompt for number of player
-		var numPlayers = 0
-		while (numPlayers < 5 || numPlayers > 10) {
-			numPlayers = prompt("How many players?", 5);
-		}
-		
-
-		// Choose rule according to number
-		this.rule = rules[numPlayers];
-
-		// Get each player's name
-		var name = undefined;
-		for (var i = 0; i < numPlayers; i++) {
-			name = prompt("What is your name, player "+(i+1)+"?", "Player"+(i+1));
-			this.players.push(new Player(name, i));
-		}
-
-		// Set spies
-		var spyCount = 0;
-		while (spyCount < this.rule.spies) {
-			var key = Math.floor(Math.random() * this.players.length);
-			if (!this.players[key].isSpy) {
-				this.players[key].isSpy = true;
-				spyCount++;
-			}
-		}
-
-		this.showRoles();
-	},
-	showRoles: function() {
-
-		for (var i = 0, c = this.players.length; i < c; i++) {
-			var player = this.players[i];
-			var role = player.isSpy ? "Spy" : "Resistant";
-
-			alert("Please hand the device to "+player.name+".\nIf you are "+player.name+", press OK.");
-			alert(player.name+", your role is: "+role+".\nPress OK before you hand the device.");
-		}
-
-		this.recognition();
-
-	},
-	recognition: function() {
-
-		alert("Spy recognition phase will begin. Be sure your speaker volume is up and press OK.");
-
-		// Every player close their eyes
-		say('To all players: please close your eyes.', 1000);
-
-		// Spies, open your eyes
-		say('Spies: please open your eyes.', 6000);
-
-		// Spies, close your eyes
-		say('Spies: please close your eyes.', 11000);
-
-		// Every player open their eyes
-		say('To all players: you may now open your eyes.', 14000);
-
-		var _this = this;
-		setTimeout(function() {
-			_this.main();
-		}, 14000)
-
-	},
-	main: function() {
-
-
-		var nextLeaderKey = 1;
-		var leader = this.players[0];
-		var nextLeader = this.players[nextLeaderKey];
-
-		var success = 0,
-			failures = 0;
-
-		// 5 mission loop
-		for (var missionNumber = 0; missionNumber < 5; missionNumber++) {
-
-			var vote = false,
-				leadersRemaining = 5;
-
-/* 			if (confirm("Exit?")) throw "Game exited"; */
-			alert("New mission!\n\nMission "+(missionNumber+1)+" of 5\nLeader: "+leader.name+"\nNext leader: "+nextLeader.name+"\n");
-
-			// Vote for leader choice acceptance
-			while (!vote) {
-				alert(leader.name+", choose "+this.rule.missions[missionNumber]+" soldiers for this mission.");
-				var vote = confirm(leader.name+", do the majority of players accept your choice of soldiers for this mission?");
-
-				if (!vote) {
-
-					leadersRemaining--;
-					if(leadersRemaining == 0)
-					{
-						alert("Game over! You ran out of leaders. The governement wins.");
-						throw "Game over!";
-					}
-
-					// Change leader & nextLeader
-					leader = nextLeader;
-					nextLeaderKey++;
-					nextLeaderKey = nextLeaderKey == this.players.length ? 0 : nextLeaderKey;
-					nextLeader = this.players[nextLeaderKey];
-					alert("New leader for mission"+(missionNumber+1)+"\n\nThe new leader is: "+leader.name+"\nNext leader will be: "+nextLeader.name+"\n\nLeaders remaining: "+leadersRemaining);
-				}
-			}
-
-			// Add player to team
-			var team = [];
-			for (var i = 0; team.length < this.rule.missions[missionNumber]; i++) {
-				if (i == this.players.length) i = 0;
-				var player = this.players[i];
-				if (confirm("Is "+player.name+" in the team for this mission?")) {
-					team.push(player);
-				}
-			}
-
-			alert("Mission begins!");
-
-			// Each player vote for mission success
-			var sabotages = 0;
-			for (var i = 0, c = team.length; i < c; i++) {
-				var player = team[i];
-				alert("Hand the phone to "+player.name+". Press OK if you are "+player.name+".");
-				if (confirm(player.name+", do you want to sabotage this mission?") && player.isSpy) {
-					sabotages++;
-				}
-			}
-
-			// Mission result
-			var missionSuccess = true;
-			if (missionNumber == 4 && this.players.length > 6)
-			{
-				if (sabotages >= 2)
-				{
-					missionSuccess = false;
-				}
-			}
-			else if (sabotages)
-			{
-				missionSuccess = false;
-			}
-
-			if (!missionSuccess) {
-				failures++;
-				var result = "Failure!";
-			} else {
-				success++;
-				var result = "Success!";
-			}
-			alert("Mission result: "+result+" ("+sabotages+" sabotages)");
-
-			// If the spies have 3 points, they win
-			if (failures == 3) {
-				alert("Game over! The governement wins.");
-				throw "Game over! The resistance wins.";
-			}
-
-			// Change leader & nextLeader
-			leader = nextLeader;
-			nextLeaderKey = nextLeaderKey >= this.players.length ? 0 : nextLeaderKey+1;
-			nextLeader = this.players[nextLeaderKey];
-
-		}
-		alert("Game over! The resistance wins.");
-	}
+	this.players = [];
+	this.spies = [];
+	this.soldiers = [];
+	this.missions = [];
+	this.currentPhase = 'menu';
+	this.currentView = 'handing';
+	this.currentMission = 0;
+	this.currentPlayer = 0;
+	this.currentLeader = undefined;
+	this.nextLeader = undefined;
+	this.changedLeader = 0;
+	this.score = {
+		resistance: 0,
+		governement: 0
+	};
+	this.result = undefined;
 }
 
 function Player(name, order) {
+	this.id = name;
 	this.name = name;
-	this.order = order;
+	this.order = Player.counter;
 	this.isSpy = false;
+	Player.counter++;
 }
+Player.counter = 0;
+
+function Mission(id, teamSize) {
+	this.id = id;
+	this.num = this.id + 1;
+	this.teamSize = teamSize;
+	this.team = [];
+	this.sabotages = 0;
+}
+
+// Main controller
+app.controller('GameController', function(uuid4) {
+	
+	// New player placeholder var
+	this.newPlayer = new Player();
+	
+	// Log
+	this.gamelog = [];
+	this.log = function(msg) {
+		console.log(msg);
+		this.game.log.push({ message: msg, date: new Date() });
+	};
+	
+	// Start a new game
+	this.start = function() {
+		this.game = new Game(uuid4.generate());
+		this.log('Game started');
+		
+		this.game.currentPhase = 'setPlayers';
+		$('#setPlayers').find('input').focus();
+		
+		this.save();
+	};
+	
+	// Save current game
+	this.save = function() {
+		localStorage.game = angular.toJson(this.game);
+	};
+	
+	// Resume a saved game
+	this.resume = function() {
+		if (localStorage.game) {
+			this.game = JSON.parse(localStorage.game);
+			this.log('Game resumed');
+		}
+		else {
+			alert('No saved game !');
+		}
+	};
+	
+	// Add a player
+	this.addPlayer = function() {
+		this.newPlayer.id = uuid4.generate();
+		this.game.players.push(this.newPlayer);
+		this.log('Added player '+this.newPlayer.name);
+		this.newPlayer = new Player();
+		
+		this.save();
+	};
+	
+	this.nextPlayer = function() {
+		this.game.currentPlayer++;
+		if (this.game.currentPlayer == this.game.players.length)
+		{
+			this.game.currentPlayer = 0;
+		}
+	};
+	
+	this.addSoldier = function(soldier) {
+		var ctrl = this;
+		
+		if (ctrl.game.currentMission.team.length >= ctrl.game.currentMission.teamSize) {
+			alert('Team is full !');
+			return false;
+		}
+		
+		// Add to the team
+		soldier.order = ctrl.game.currentMission.team.length;
+		ctrl.game.currentMission.team.push(soldier);
+		
+		// Remove from available soldiers
+		$.each(this.game.soldiers, function(index, item) {
+			if (item.id == soldier.id) {
+				ctrl.game.soldiers.splice(index, 1);
+				return false;
+			}
+		});
+		
+	};
+	
+	this.removeSoldier = function(soldier) {
+	
+		var ctrl = this;
+		
+		// Add to available soldiers
+		ctrl.game.soldiers.push(soldier);
+		
+		// Remove from the team
+		$.each(this.game.currentMission.team, function(index, item) {
+			if (item.id == soldier.id) {
+				ctrl.game.currentMission.team.splice(index, 1);
+				return false;
+			}
+		});
+		
+	};
+	
+	this.nextLeader = function() {
+		
+		if (this.game.changedLeader >= 5) {
+			this.log('The resistance ran out of leader. The governement wins.');
+			this.endGame('The resistance ran out of leader. The governement wins.');
+		} 
+		else 
+		{
+		
+			// Set the new current Leader
+			this.game.currentLeader = this.game.nextLeader;
+			this.log('Changing leader to '+this.game.currentLeader.name);
+			
+			// Set the new next Leader
+			var nextLeaderKey = this.game.currentLeader.order + 1;
+			nextLeaderKey = (nextLeaderKey == this.game.players.length) ? 0 : nextLeaderKey;
+			this.game.nextLeader = this.game.players[nextLeaderKey];
+			
+			this.game.changedLeader++;
+			
+			this.save();
+		}
+	};
+	
+	// Set rule & spies
+	this.setSpies = function() {
+		
+		var game = this.game;
+		
+		// Choose rule according to number
+		game.rule = rules[game.players.length];
+
+		// Create each game mission
+		for (var i = 0; i < 5; i++) {
+			game.missions.push(new Mission(i, game.rule.missions[i]));
+		}
+		
+		// Set spies
+		var spyCount = 0, spies = [];
+		while (spyCount < game.rule.spies) {
+			var key = Math.floor(Math.random() * game.players.length);
+			if (!game.players[key].isSpy) {
+				this.game.players[key].isSpy = true;
+				this.game.spies.push(this.game.players[key].name);
+				spyCount++;
+			}
+		}
+		
+		this.log('Randomly picked '+this.game.rule.spies+' spies: '+this.game.spies.join());
+		
+		// Setting game start params
+		this.game.currentMission = this.game.missions[0];
+		this.game.currentLeader = this.game.players[0];
+		this.game.nextLeader = this.game.players[1];
+		
+		this.showRoles();
+	};
+	
+	this.showRoles = function() {
+		
+		this.game.currentPhase = 'showRoles';
+		this.game.currentView = 'handing';
+		this.game.currentPlayer = 0;
+		
+		this.save();
+		
+	};
+	
+	this.prepareMission = function() {
+		
+		var game = this.game;
+		
+		// Clone players array
+		game.soldiers = game.players.slice(0);
+		
+		game.currentPhase = "prepareMission";
+		game.currentView = "teamVote";
+		
+		this.save();
+	};
+	
+	this.doMission = function() {
+		
+		var game = this.game,
+			mission = game.currentMission;
+		
+		this.log('Doing mission '+mission.num);
+		
+		game.currentPhase = "doMission";
+		game.currentView = "handing";
+		
+		game.currentSoldier = mission.team[0];
+			
+		this.save();
+	};
+	
+	this.missionVote = function(sabotage) {
+		
+		var game = this.game,
+			mission = game.currentMission;
+		
+		if (sabotage) {
+			this.log(game.currentSoldier.name+" choose to sabotage mission "+mission.num+".");
+			mission.sabotages++;
+		}
+		
+		// Go to next soldier;
+		var nextSoldierKey = game.currentSoldier.order + 1;
+		game.currentSoldier = mission.team[nextSoldierKey];
+		
+		if (game.currentSoldier) {
+			game.currentView = "handing";
+		} else {
+			// Show mission outcome
+			if (mission.sabotages) {
+				game.score.governement++;
+			}
+			else {
+				game.score.resistance++;
+			}
+			game.currentPhase = "missionResult";
+		}
+		
+		this.save();
+	};
+	
+	this.nextMission = function() {
+	
+		var game = this.game,
+			mission = game.currentMission;
+			nextMissionId = mission.id + 1;
+		
+		if (game.score.governement >= 3) {
+			this.endGame('Three missions failed. The governement wins.');
+		}
+		else if (game.score.resistance >= 3) {
+			this.endGame('Three missions have succeeded. The resistance wins.');
+		}
+		else {
+			game.currentMission = game.missions[nextMissionId];
+			this.prepareMission();
+		}
+		
+	};
+	
+	this.endGame = function(result) {
+		this.log(result);
+		this.game.currentPhase = "gameOver";
+		this.game.result = result;
+		
+		this.save();
+	};
+});
 
 var rules = {
 	5: {
@@ -206,20 +318,4 @@ var rules = {
 		spies: 4,
 		missions: [3, 4, 4, 5, 5]
 	}
-}
-
-function say(message, timeout) {
-	var to = timeout || 0;
-	setTimeout(function() {
-		var msg = new SpeechSynthesisUtterance();
-		msg.text = message;
-		msg.lang = 'en-US';
-		msg.rate = 0.25;
-		window.speechSynthesis.speak(msg);
-	}, timeout);
-}
-
-function newGame() {
-	var game = new Game();
-	game.start();
-}
+};
